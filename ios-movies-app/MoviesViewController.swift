b1144b03e3ac267c4f0f9f7a94ad24dd85702cf1
Yayang Tian <ytian@walmartlabs.com>
2015-09-14 22:31:12 -0700

    Added basis.

---
+++ ios-movies-app/MoviesViewController.swift
@@ -0,0 +1,88 @@
+//
+//  MoviesViewController.swift
+//  ios-movies-app
+//
+//  Created by Yayang Tian on 9/14/15.
+//  Copyright © 2015 walmartlabs. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    var movies: NSArray?
+    
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let url = NSURL(string: "https://gist.githubusercontent.com/timothy1ee/d1778ca5b944ed974db0/raw/489d812c7ceeec0ac15ab77bf7c47849f2d1eb2b/gistfile1.json")!;
+        
+        let request  = NSURLRequest(URL: url)
+       
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()){
+            (request, data, error) -> Void in
+                
+            let json = try! NSJSONSerialization.JSONObjectWithData(data!, options: []) as! NSDictionary
+            self.movies = json["movies"] as? NSArray
+            print(self.movies)
+            self.tableView.reloadData()
+        }
+
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
+
+    func get(url: NSURL, completion: (NSDictionary) -> ()) {
+        let request  = NSURLRequest(URL: url)
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            data, response, error in
+            if error != nil {
+                print("error: \(error)")
+                return
+            } else {
+                let json = try! NSJSONSerialization.JSONObjectWithData(data!, options: []) as! NSDictionary
+                completion(json)
+            }
+        }
+        task.resume()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let movies = movies {
+            return movies.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as UITableViewCell
+         
+        let movie = movies![indexPath.row]
+        cell.textLabel?.text = movie["title"] as? String
+        
+        return cell
+
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ios-movies-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-movies-app
+//
+//  Created by Yayang Tian on 9/14/15.
+//  Copyright © 2015 walmartlabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
